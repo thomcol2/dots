b7c181af6839770d091a363e6f9f3cfad5e93e22
Cole Thompson <43302913+thomcol2@users.noreply.github.com>
2025-06-13 16:57:08 -0500

    better package install

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -9,11 +9,13 @@ if [ $CWD != ".dots" ]; then
 fi
 
 
-# Install basic arch packages
-pacman -S - < $CWD/packages/base_package_list.txt
-pacman -S - < $CWD/packages/programming_list.txt
-# pacman -S - < $CWD/packages/software_package_list.txt
+# Install arch packages
+pacman -S --needed - < $CWD/packages/base_package_list.txt
+pacman -S --needed - < $CWD/packages/programming_list.txt
+# pacman -S --needed - < $CWD/packages/software_package_list.txt
 
+
+# Enable and start services
 systemctl start pipewire.service
 systemctl start wireplumber.service
 systemctl start bluetooth.service
@@ -23,17 +25,19 @@ systemctl enable wireplumber.service
 systemctl enable bluetooth.service
 systemctl enable iwd.service
 
+
+# Install suckless packages
 suckless_build() {
     echo "\n##### Building: $1 #####\n"
     cd $CWD/.config/$1
     make clean install || echo "##### $1 build failed #####"
 }
 
-# Install suckless packages
 suckless_build dwm
 suckless_build dwmblocks
 suckless_build dmenu
 suckless_build st
 suckless_build slock
+
 
 echo "\nGraphics drivers must be installed separately\n"
